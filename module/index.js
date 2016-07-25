a73fcf92dc528ce06def4b7fad8f561bef117c8d
Yves Riedener <git@reecube.com>
2016-07-25 21:56:35 +0200

    implemented the public files

--- module/index.js
+++ module/index.js
@@ -3,10 +3,11 @@
 let Core = require('./index-core'),
     core = Core(),
     pgo = require('pokemon-go-node-api'),
+    pokedex = require('./data/pokedex.json'),
     express = require('express'),
     app = express();
 
-app.use(express.static('public'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     if (req.body && req.body.length) {
